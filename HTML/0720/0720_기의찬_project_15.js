e09e2a17e50969ecf94dbfc7925378d6c9a2b372
KeeUi0 <77387339+KeeUi0@users.noreply.github.com>
2022-07-21 17:50:11 +0900

    21_1

--- HTML/0720/0720_기의찬_project_15.js
+++ HTML/0720/0720_기의찬_project_15.js
@@ -1,11 +1,44 @@
-var bigPic = document.querySelector("#main_img");
-var smallPics = document.querySelectorAll("#photos");
+$('div > div > img#move_left').click(function(){
+    var $this = $(this);
+    var $slider = this.closest('.slider');
+    
+    var index = $this.index();
+    var isLeft = index == 0;
 
-for(var i = 0 ; i < smallPics,length ; i++) {
-    smallPics[i].addEventListener("click", changepic);
-}
+    var $current = $slider.find('> div > div > img#move_left:active');
+    var $post;
 
-function changepic() {
-    var smallPicsAttribute = this. getAttribute("src");
-    bigPic.setAttribute("src", smallPicsAttribute);
-}
+    if ( isLeft){
+        $post = $current.prev();
+    }
+    else {
+        $post = $current.next();
+    }
+
+    if ( $post.lenght == 0) {
+        if (isLeft) {
+            $post = $slider.find (' > div > div > img#move_left > #photos:last-child ')
+        }
+    }
+})
+
+
+console.clear();
+
+// 기존 버튼형 슬라이더
+$('div > div > ul#photos img').click(function(){
+    var $this = $(this);
+    var index = $this.index();
+    
+    $this.addClass('active');
+    $this.siblings('.active').removeClass('active');
+    
+    var $slider = $this.parent().parent();
+    
+    var $current = $slider.find('div > div > ul#photos img:active');
+    
+    var $post = $slider.find(' > .slides > div').eq(index);
+    
+    $current.removeClass('active');
+    $post.addClass('active');
+});
---
+++ HTML/0721/lib.js
@@ -0,0 +1,7 @@
+//자바 스크립트 외부 링크
+function over(obj) {
+    obj.src = "C:\\Users\\hi-guro\\Desktop\\keeui\\java_fullstack\\media\\banana.png";
+}
+function out(obj) {
+    obj.src = "C:\\Users\\hi-guro\\Desktop\\keeui\\java_fullstack\\media\\apple.png";
+}
