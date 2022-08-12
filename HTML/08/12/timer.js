47ac3983574dc871fa6bd62649482124e1e4b02e
KeeUi0 <77387339+KeeUi0@users.noreply.github.com>
2022-08-12 18:14:00 +0900

    12_3

--- HTML/08/12/timer.js
+++ HTML/08/12/timer.js
@@ -1,15 +1,23 @@
 var count = 0; 
 var timerID = null;
+var k =0;
 
 
 onmessage = function(e) {
     str = e.data;
     if(str == "start"){
         timerID = setInterval(myCallback, 1000);
+        k++;
+        return;
+
     }
     if(str == "stop"){
         clearInterval(timerID);
         return;
+    }
+    if(k > 1){
+        timerID = function start(){}
+        stop(start());
     }
 }
     
@@ -23,3 +31,5 @@ function myCallback() { //1초 간격으로 호출
 
 
 //setInterval() 이용해서 만들어보세요.
+// timerID와 return을 잘 활용하여
+// start 버튼이 최초에 눌린 이후에 동작하지 않도록
